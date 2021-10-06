5a74077ae38fc84f87335502f84a350d9cd22546
hyunrice98 <hyunrice98@gmail.com>
2021-10-06 23:02:42 +0900

    week2

--- Week2_1_12171712_JaehyunCho.cpp
+++ Week2_1_12171712_JaehyunCho.cpp
@@ -1,4 +1,52 @@
 //
+// W2_1: Matching_Sentences
 // Created by 조재현 on 2021/09/10.
 //
 
+#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int t, n, m, k, answer;
+string s;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> t;
+    while (t--) {
+        cin >> n >> m;
+
+        string sentence[2001];
+        queue<string> word[21];
+
+        for (int i = 0; i < m; i++) {
+            cin >> sentence[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> k;
+            for (int j = 0; j < k; j++) {
+                cin >> s;
+                word[i].push(s);
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            answer = 0;
+            for (int j = 0; j < n; j++) {
+                if (sentence[i] == word[j].front()) {
+                    word[j].pop();
+                    answer = 1;
+                    break;
+                }
+            }
+            if (answer == 0) break;
+        }
+        cout << answer << '\n';
+    }
+
+    return 0;
+}
--- Week2_2_12171712_JaehyunCho.cpp
+++ Week2_2_12171712_JaehyunCho.cpp
@@ -1,4 +1,46 @@
 //
+// W2_2: Securing_Resources
 // Created by 조재현 on 2021/09/10.
 //
 
+#include <iostream>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+int t, n, answer, flag;
+int resourceA[10001];
+int resourceB[10001];
+pair<int, int> resourceSum[10001];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cin >> resourceA[i] >> resourceB[i];
+            resourceSum[i] = make_pair(resourceA[i] + resourceB[i], i);
+        }
+        sort(resourceSum, resourceSum + n);
+        answer = 0;
+        flag = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (flag == 1) {
+                answer += resourceA[resourceSum[i].second];
+                flag = 0;
+            } else if (flag == 0) {
+                answer -= resourceB[resourceSum[i].second];
+                flag = 1;
+            }
+        }
+        cout << answer << '\n';
+    }
+
+    return 0;
+}
